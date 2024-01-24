dbed2a027c8ff1fcbac457c33eb30e695cc8afdb
Lazy Cat <mylazysundaycat@gmail.com>
2024-01-24 19:35:21 +0900

    섹션6 요청 매핑

---
+++ src/main/java/hello/springmvc2/basic/LogTestController.java
@@ -0,0 +1,36 @@
+package hello.springmvc2.basic;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+@Controller 애노테이션은
+스프링의 리턴값이 String일시에 뷰 논리이름으로 인식한다.
+@RestController 애노테이션이 붙으면
+문자가 반환되면 스트링이 그대로 출력된다.
+ */
+@RestController
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest(){
+        String name ="Spring";
+        System.out.println("name = "+name);
+
+        //로그 상태 레벨
+        //모든 로그를 보고싶다면?
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name); //디버그, 개발서버 로그
+        log.info("info log={}", name); //비즈니스 정보
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+
+
+}
