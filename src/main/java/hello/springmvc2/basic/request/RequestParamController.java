1e338bf53a2093f021f09e9c5433e7292d2dfacc
Lazy Cat <mylazysundaycat@gmail.com>
2024-01-27 17:09:50 +0900

    섹션6 API 예시3

--- src/main/java/hello/springmvc2/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc2/basic/request/RequestParamController.java
@@ -1,2 +1,78 @@
-package hello.springmvc2.basic.request;public class RequestParamController {
+package hello.springmvc2.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.Map;
+
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request,
+                               HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age
+    ) {
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+        return "ok";
+    }
+
+    //기본 값 적용
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParmaDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age
+    ) {
+        return "ok";
+    }
+
+    //파라미터를 Map으로 조회
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        return "ok";
+    }
+
+    
 }
