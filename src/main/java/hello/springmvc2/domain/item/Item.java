1b09bc893afef75ff374c959ef31f412adadacab
Lazy Cat <mylazysundaycat@gmail.com>
2024-01-31 15:38:08 +0900

    섹션7 상품 도메인 개발

---
+++ src/main/java/hello/springmvc2/domain/item/Item.java
@@ -0,0 +1,21 @@
+package hello.springmvc2.domain.item;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item(){}
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
---
+++ src/test/java/hello/springmvc2/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,50 @@
+package hello.springmvc2.domain.item;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach(){
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save(){
+        //given
+        Item item = new Item("A",10000,10);
+
+        //when
+        Item savedItem = itemRepository.save(item);
+
+        //then
+        Item findItem = itemRepository.findById(item.getId());
+        assertThat(findItem).isEqualTo(savedItem);
+    }
+
+    @Test
+    void findAll(){
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void updateItem(){
+        //given
+
+        //when
+
+        //then
+    }
+
+}
